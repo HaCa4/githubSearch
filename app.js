8fe41c8623a855267acc8fbdc012e32327801b7d
unknown <hcozagar@gmail.com>
2022-04-19 14:20:28 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const githubForm=document.getElementById("github-form");
+const nameInput=document.getElementById("githubname");
+const clearLastUsers=document.getElementById("clear-last-users");
+const lastUsers=document.getElementById("last-users");
+const github=new Github();
+const ui=new UI();
+
+eventListeners();
+function eventListeners(){
+    githubForm.addEventListener("submit",getData);
+    clearLastUsers.addEventListener("click",clearAllSearched);
+    document.addEventListener("DOMContentLoaded",getAllSearched);
+}
+
+function getData(e){
+    let username=nameInput.value.trim();
+    if(username===""){
+        alert("Lütfen geçerli bir kullanıcı adı girin");
+    }
+    else{
+        github.getGithubData(username)
+        .then(response=>{
+            if(response.user.bio===null){
+                ui.showError("Kullanıcı bulunamadı")
+            }
+            else{
+                ui.addSearchedUserToUI(username);
+                Storage.addSearchedUserToStorage(username);
+                ui.showUserInfo(response.user);
+                ui.showRepoInfo(response.repo);
+            }
+        })
+        .catch(err=>console.log(err))
+
+    }
+    ui.clearInput();                                                    
+    e.preventDefault()
+}
+function clearAllSearched(){                                            //arananları temizle
+    if(confirm("Emin Misiniz?")){
+        Storage.clearAllSearchedUsersFromStorage();
+        ui.clearAllSearchedUsersFromUI();
+        }
+    }
+function getAllSearched(){                                              //arananları storage'dan al ve ui'ye ekle
+    let users=Storage.getSearchedUsersFromStorage();
+    let result="";
+    users.forEach(user=>{
+        result+=`<li class="list-group-item bg-warning">${user}</li>`;
+    });
+    lastUsers.innerHTML=result;
+}
